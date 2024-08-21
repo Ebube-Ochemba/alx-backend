591db851fabbecafcd3398b2cfe2568c950e31d9
Ebube-Ochemba <ebube17@gmail.com>
2024-08-21 23:09:20 +0100

    done

---
+++ 0x01-caching/1-fifo_cache.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+"""1-fifo_cache
+A dictionary to illustrate FIFO
+"""
+from base_caching import BaseCaching
+
+
+class FIFOCache(BaseCaching):
+    def __init__(self):
+        """Initialize the cache"""
+        super().__init__()
+        self.order = []  # To keep track of the order of keys
+
+    def put(self, key, item):
+        """Add an item to the cache with FIFO eviction"""
+        if key is None or item is None:
+            return
+
+        if key in self.cache_data:
+            # If the key already exists, update the order...
+            self.order.remove(key)
+
+        self.cache_data[key] = item
+        self.order.append(key)  # Add key to the end of the order list
+
+        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+            # If the cache exceeds MAX_ITEMS, evict the first item added (FIFO)
+            first_key = self.order.pop(0)  # remove 1st item in the order list
+            del self.cache_data[first_key]  # Remove it from the cache
+            print(f"DISCARD: {first_key}")
+
+    def get(self, key):
+        """Retrieve an item from the cache"""
+        return self.cache_data.get(key) if key is not None else None
