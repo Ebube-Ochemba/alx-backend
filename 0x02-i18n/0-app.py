bcb2158fadc69124a66d361cb2c4d185e98a4204
Ebube-Ochemba <ebube17@gmail.com>
2024-08-27 22:41:15 +0100

    annotation

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 """Entry point of the web application"""
 from flask import Flask, render_template
+from typing import Any
 
 app = Flask(__name__)
 
 
-@app.route("/")
-def index():
+@app.route("/", strict_slashes=False)
+def index() -> Any:
     """renders a simple template """
     return render_template("0-index.html")
 
