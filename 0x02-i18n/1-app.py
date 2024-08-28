97b85f0190efc2932d7206a1a1c95ccf8d207f89
Ebube-Ochemba <ebube17@gmail.com>
2024-08-28 01:00:34 +0100

    correct template

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -25,7 +25,7 @@ babel = Babel(app)
 @app.route("/", strict_slashes=False)
 def index() -> Any:
     """renders a simple template"""
-    return render_template("0-index.html")
+    return render_template("1-index.html")
 
 
 if __name__ == "__main__":
