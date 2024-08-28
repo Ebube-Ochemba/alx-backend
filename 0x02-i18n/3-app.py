79d1ec2305c82a34ca22b67d418aaff487bf4c35
Ebube-Ochemba <ebube17@gmail.com>
2024-08-28 02:03:58 +0100

    gettext as _

---
+++ 0x02-i18n/3-app.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+"""Entry point of the web application"""
+from flask import Flask, render_template, request
+from flask_babel import Babel, gettext as _
+from typing import Any
+
+app = Flask(__name__)
+
+
+class Config:
+    """Configuration class for Babel"""
+
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+# setup config for app
+app.config.from_object(Config)
+
+# Instantiate Babel
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale() -> str:
+    """Determine the best match with our supported languages."""
+    # Use request.accept_languages to determine the best match
+    return request.accept_languages.best_match(app.config["LANGUAGES"])
+
+
+@app.route("/", strict_slashes=False)
+def index() -> Any:
+    """renders a simple template"""
+    return render_template("2-index.html")
+
+
+if __name__ == "__main__":
+    app.run()
